d51bc80a9ae2690d9599501b76aeee264fa947ca
punzalan223 <punzalan2233@gmail.com>
2025-01-11 11:43:38 +0800

    add underscore to import

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,6 +1,6 @@
 import CarbonSense from '../img/projects/carbon_sense.jpg'
-import Devp from '../img/projects/Devp system.jpg'
-import Egc from '../img/projects/EGC system.jpg'
+import Devp from '../img/projects/Devp_system.jpg'
+import Egc from '../img/projects/EGC_system.jpg'
 import FaceRecognition from '../img/projects/face_recognition.jpg'
 import FilipinoClassifieds from '../img/projects/filipino-classifieds.png'
 import Gis from '../img/projects/gis.png'
